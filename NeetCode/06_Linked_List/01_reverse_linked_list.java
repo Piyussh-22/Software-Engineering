4fbf857c0ce49627a42e30928416f64832155eb6
Piyush Raj <piyushraj4734@gmail.com>
2026-01-06 20:58:33 +0530

    linked list cycle

--- NeetCode/06_Linked_List/01_reverse_linked_list.java
+++ NeetCode/06_Linked_List/01_reverse_linked_list.java
@@ -22,6 +22,25 @@ class Solution {
     }
 }
 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
+        Stack<ListNode> stack = new Stack<>();
+        while(head != null){
+            stack.push(head);
+            head = head.next;
+        }
+        head = stack.pop();
+        ListNode temp; // temp can store the address of a listnode object.
+        temp = head;
+        while(!stack.isEmpty()){
+            temp.next = stack.pop();
+            temp = temp.next;
+        }
+        temp.next = null;
+        return head;
+    }
+}
 
 /*206. Reverse Linked List
 Easy
@@ -30,20 +49,13 @@ premium lock icon
 Companies
 Given the head of a singly linked list, reverse the list, and return the reversed list.
 
-
-
 Example 1:
-
-
 Input: head = [1,2,3,4,5]
 Output: [5,4,3,2,1]
 Example 2:
-
-
 Input: head = [1,2]
 Output: [2,1]
 Example 3:
-
 Input: head = []
 Output: []
 
