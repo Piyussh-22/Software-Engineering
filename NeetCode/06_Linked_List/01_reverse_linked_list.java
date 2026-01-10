3814d260142f8f9b05b15168e29edc2763bc858f
Piyush Raj <piyushraj4734@gmail.com>
2026-01-10 23:57:01 +0530

    linked list cycle

--- NeetCode/06_Linked_List/01_reverse_linked_list.java
+++ NeetCode/06_Linked_List/01_reverse_linked_list.java
@@ -10,17 +10,19 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
         ListNode prev = null;
-        ListNode curr = head;
-        while(curr != null){
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+        ListNode front;
+        while(head != null){
+            front = head.next;
+            head.next = prev;
+            prev = head ;
+            head = front;
         }
         return prev;
     }
 }
+
 
 class Solution {
     public ListNode reverseList(ListNode head) {
